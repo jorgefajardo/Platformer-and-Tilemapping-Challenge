382196468cc88e0dcd1ea17d6d836a65bf650972
jorgefajardo <jorge.fajardo8888@gmail.com>
2022-10-19 17:41:45 -0500

    Tier2

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -12,10 +12,18 @@ public class PlayerScript : MonoBehaviour
     public Text score;
     public Animator animator;
     private int scoreValue = 0;
-    public bool jump;
+    public bool jump, playing;
+    public Image healt;
+    public float healtN = 1;
+    public GameObject button;
+
+    public Sounds sounds;
+    public Transform init;
     // Start is called before the first frame update
     void Start()
     {
+        playing = true;
+        button.SetActive(false);
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
     }
@@ -23,55 +31,144 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float hozMovement = Input.GetAxis("Horizontal");
-        float vertMovement = Input.GetAxis("Vertical");
-        if (hozMovement > 0)
+        if (playing)
         {
-            if (!jump)
-                animator.SetInteger("Movement", 1);
-            transform.localScale = new Vector3(10, 10, 10);
+            float hozMovement = Input.GetAxis("Horizontal");
+            if (hozMovement > 0)
+            {
+                if (!jump)
+                {
+                    animator.SetInteger("Movement", 1);
+                    transform.localScale = new Vector3(10, 10, 10);
+                }
+
+            }
+            else if (hozMovement < 0)
+            {
+                if (!jump)
+                {
+                    animator.SetInteger("Movement", 1);
+                    transform.localScale = new Vector3(-10, 10, 10);
+                }
+
+            }
+            else
+            {
+                if (!jump)
+                    animator.SetInteger("Movement", 0);
+            }
+            rd2d.AddForce(new Vector2(hozMovement * speed, 0));
         }
-        else if (hozMovement < 0)
-        {
-            if (!jump)
-                animator.SetInteger("Movement", 1);
-            transform.localScale = new Vector3(-10, 10, 10);
-        }
-        else
-        {
-            if (!jump)
-                animator.SetInteger("Movement", 0);
-        }
-        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
+
+        if (collision.collider.tag == "Ground" && playing)
+        {
+            jump = false;
+        }
+
         if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            sounds.Coin();
 
             if (scoreValue == 4)
             {
+                sounds.Win();
+                animator.SetInteger("Movement", 0);
+                transform.position = init.position;
+                sounds.Start();
+                Healty();
+            }
+            if (scoreValue == 8)
+            {
                 score.text = "You Win Fajardo";
+                sounds.Win();
+                playing = false;
+                animator.SetInteger("Movement", 0);
+
+            }
+        }
+        else if (collision.collider.tag == "Enemy")
+        {
+            if (playing)
+            {
+                sounds.Enemy();
+                Destroy(collision.collider.gameObject);
+                healtN = healtN - 0.33f;
+                healt.fillAmount = healtN;
+                if (healtN < 0.1f)
+                {
+                    score.text = "You Lose";
+                    button.SetActive(true);
+                    playing = false;
+                    sounds.Llose();
+                    animator.SetInteger("Movement", 0);
+
+                }
             }
         }
 
     }
+    public void Healty()
+    {
+        sounds.Enemy();
+        healtN = 1;
+        healt.fillAmount = healtN;
+       
+    }
+    public void Healt()
+    {
+        sounds.Enemy();
+        healtN = healtN - 0.33f;
+        healt.fillAmount = healtN;
+        if (healtN < 0.1f)
+        {
+            score.text = "You Lose";
+            button.SetActive(true);
+            playing = false;
+            sounds.Llose();
+            animator.SetInteger("Movement", 0);
+
+        }
+    }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground" && playing)
         {
             if (Input.GetKey(KeyCode.W))
             {
                 animator.SetInteger("Movement", 2);
-                rd2d.velocity = new Vector2(0, jumpForce);
-                jump = true;
+                rd2d.velocity = new Vector2(rd2d.velocity.x, jumpForce);
+                Invoke("WaitJump", 1f);
             }
-            jump = false;
         }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground" && playing)
+        {
+            jump = true;
+            animator.SetInteger("Movement", 2);
+        }
+
+    }
+
+
+
+    private void WaitJump()
+    {
+    }
+
+    public void ResetScene()
+    {
+        Application.LoadLevel(Application.loadedLevel);
     }
 }
